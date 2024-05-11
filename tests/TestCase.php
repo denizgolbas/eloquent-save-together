d6aef778bad36e1a93fbc04685df43455f27b515
Deniz Gölbaş <golbacyan@gmail.com>
2024-05-12 00:33:28 +0300

    Test wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,7 @@
 
 namespace Denizgolbas\EloquentSaveTogether\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Denizgolbas\EloquentSaveTogether\EloquentSaveTogetherServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -12,15 +10,12 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Denizgolbas\\EloquentSaveTogether\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            EloquentSaveTogetherServiceProvider::class,
+
         ];
     }
 
@@ -28,9 +23,5 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_eloquent-save-together_table.php.stub';
-        $migration->up();
-        */
     }
 }
